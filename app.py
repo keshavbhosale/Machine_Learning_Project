2d2447893161180014342b7b835fc3294c0f0fa8
Keshav Bhosale <keshavbhosale14571@gmail.com>
2022-10-31 11:34:11 +0530

    updated logger and exception

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
+import sys
+import housing
+from housing.logger import logging
+from housing.exception import Housing_Exception
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing=Housing_Exception(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logger ")
     return 'Starting machine learning project'
 
 
